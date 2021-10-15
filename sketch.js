1c9d6f63c5c141396834536afc9e857cef19abce
Gamerbro05 <92558460+Gamerbro05@users.noreply.github.com>
2021-10-14 18:52:27 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200, 200, 40, 40)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown("left")){
+    box.x -= 2
+  }
+  if(keyDown("right")){
+    box.x += 2
+  }
+  if(keyDown("up")){
+    box.y -= 2
+  }
+  if(keyDown("down")){
+    box.y += 2
+  }
+drawSprites()
+}
+
+
+
+
